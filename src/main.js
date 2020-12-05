acf60f2eb9ae48812625edbf4fe3a2c2276a8981
kazushi-takagaki <tkgkkzs@gmail.com>
2020-12-05 18:38:44 +0900

    commit vuerouter+vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from "./router";
+import store from "./store";
 
 Vue.config.productionTip = false
 
+router.beforeEach((to, from, next) => {
+  if (router.path === "/users/1") {
+    next("/");
+  }
+  next();
+});
+
 new Vue({
+  router: router,
+  store: store,
   render: h => h(App),
 }).$mount('#app')
